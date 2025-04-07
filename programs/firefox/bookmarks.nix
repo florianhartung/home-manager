6033194eb02266994f9de4c51b21a86b9f049ee1
Florian Hartung <florian.hartung@dlr.de>
2025-04-07 10:29:56 +0200

    feat(firefox): add bookmark for ublock origin rules page

--- programs/firefox/bookmarks.nix
+++ programs/firefox/bookmarks.nix
@@ -9,4 +9,9 @@
     keyword = "std";
     url = "https://doc.rust-lang.org/std/";
   }
+  {
+    name = "uBlock Origin rules";
+    keyword = "rules";
+    url = "moz-extension://62fc4545-e66a-4d55-b3f6-af370b2feb96/dashboard.html#dyna-rules.html";
+  }
 ]
