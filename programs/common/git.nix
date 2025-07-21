44d49ba95db6eacbedc66c35908f661e204125d3
Florian Hartung <florian.hartung@dlr.de>
2025-07-21 17:53:02 +0200

    feat: git anonymous email

--- programs/common/git.nix
+++ programs/common/git.nix
@@ -3,7 +3,7 @@
   programs.git = {
     enable = true;
     userName = "Florian Hartung";
-    userEmail = "florian.hartung@dlr.de";
+    userEmail = "60144801+florianhartung@users.noreply.github.com";
     lfs.enable = true;
     extraConfig = {
       init.defaultBranch = "main";
