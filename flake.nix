d0f525badd5cd54a56cee33f5d26d5d486fd9598
Florian Hartung <60144801+florianhartung@users.noreply.github.com>
2026-03-31 15:40:14 +0200

    feat: formatter and clean up

--- flake.nix
+++ flake.nix
@@ -2,13 +2,22 @@
   description = "Home Manager configuration of flo";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
 
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
@@ -20,30 +29,69 @@
     };
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, home-manager, firefox-addons, kak-tree-sitter-helix, ... }:
-    let
-      system = "x86_64-linux";
-      my-lib = import ./lib { inherit (nixpkgs) lib; };
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      pkgs-unstable = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
-      homeConfigurations."flo" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          kak-tree-sitter-helix.homeManagerModules.${system}.kak-tree-sitter-helix
-          ./users/flo.nix
-        ];
-        extraSpecialArgs = { inherit firefox-addons pkgs-unstable my-lib; };
-      };
-      homeConfigurations."hart_fo" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./users/work.nix ];
-        extraSpecialArgs = { inherit firefox-addons my-lib; };
-      };
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      devshell,
+      flake-utils,
+      treefmt-nix,
+      firefox-addons,
+      kak-tree-sitter-helix,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        my-lib = import ./lib { inherit (nixpkgs) lib; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        homeConfigurations."flo" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            kak-tree-sitter-helix.homeManagerModules.${system}.kak-tree-sitter-helix
+            ./users/flo.nix
+          ];
+          extraSpecialArgs = { inherit firefox-addons pkgs-unstable my-lib; };
+        };
+        homeConfigurations."hart_fo" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./users/work.nix ];
+          extraSpecialArgs = { inherit firefox-addons my-lib; };
+        };
+
+        devShells.default = (
+          pkgs.devshell.mkShell {
+            name = "dev";
+
+            packages = with pkgs; [
+              nixd # Nix LSP
+            ];
+
+            imports = [ "${devshell}/extra/git/hooks.nix" ];
+
+            git.hooks = {
+              enable = true;
+              pre-commit.text = "nix flake check";
+            };
+          }
+        );
+
+        formatter = treefmtEval.config.build.wrapper;
+
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+      }
+    );
 }
---
+++ treefmt.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  # Used to find the project root
+  projectRootFile = "flake.nix";
+
+  programs.nixfmt.enable = true;
+  programs.prettier = {
+    enable = true;
+    includes = [ "*.md" ];
+  };
+}
