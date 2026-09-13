21b44b06d284d90dceb685cd375e5770c4820790
Florian Hartung <60144801+florianhartung@users.noreply.github.com>
2026-09-13 18:11:14 +0200

    update: to home-manager/master

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 
     # for home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     firefox-addons = {
